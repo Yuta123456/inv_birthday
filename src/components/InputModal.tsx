83440c63853e49de41d6fd37a278e5df5a075557
Yuta123456 <yuuta09090530@icloud.com>
2022-05-14 14:13:36 +0900

    基本機能を実装

---
+++ src/components/InputModal.tsx
@@ -0,0 +1,86 @@
+import { SettingsPowerRounded } from "@mui/icons-material";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { Box, Button, Input, Modal, TextField, Typography } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { ja } from "date-fns/locale";
+import { useState } from "react";
+
+export default function InputModal(props) {
+    const [birthDay, setBirthDay] = useState("hu May 12 2022 00:00:00 GMT+0900 (日本標準時)");
+    const [friendName, setFriendName] = useState("");
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+        props.setFriendName("");
+        props.setInvBirthDay("");
+    };
+    const handleClose = () => {
+        setOpen(false);
+        setFriendName("");
+    };
+    return (
+        <>
+        <Button onClick={handleOpen}>計算する</Button>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            // FIXME: ここでエラーが出る
+            children={<></>}
+            >
+                
+            <Box sx={boxStyle}>
+                <div style={inputForm}>
+                    <Typography sx={{padding:"0 10px 0 0"}}>友達の名前：</Typography>
+                    <Input onChange={(e) => {setFriendName(e.target.value)}}
+                           inputProps={{maxLength:10}}
+                    ></Input>
+                </div>
+                <div style={inputForm}>
+                    <Typography sx={{flex: "1 1", padding:"0 10px 0 0"}}>
+                        {friendName}の前回の誕生日を入力
+                    </Typography>
+                    <LocalizationProvider dateAdapter={AdapterDateFns} locale={ja}>
+                        <DatePicker
+                            label={friendName+"の誕生日"}
+                            value={birthDay}
+                            onChange={(newValue) => {
+                                setBirthDay(newValue);
+                                console.log(newValue);
+                                // console.log(typeof(newValue));
+                            }}
+                            inputFormat='yyyy年MM月dd日'
+                            mask='____年__月__日'
+                            renderInput={(params) => <TextField {...params} />}
+                            DialogProps={{sx:datePickerStyle}}
+                        />
+                    </LocalizationProvider>
+                </div>
+                <div style={{textAlign:'right'}}>
+                    <Button onClick={() => {handleClose();props.calcInvBirthDay(birthDay, friendName)}} >OK</Button>
+                </div>
+            </Box>
+        </Modal>
+        </>
+    )
+}
+
+const boxStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+    width: "fit-content"
+};
+const inputForm = {
+    display: "flex",
+    margin: "15px 0px",
+}
+
+const datePickerStyle = {
+    flex: "1 1 30%",
+    margin: "10px",
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,36 @@
-import { Button, TextField } from '@mui/material';
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { Button, TextField, Typography } from '@mui/material';
+import Container from '@mui/material/Container';
 import React, { useState, useEffect } from 'react';
 import ja from 'date-fns/locale/ja'
+import InputModal from '../components/InputModal';
+import format from 'date-fns/format';
+import { addDays } from 'date-fns';
 export default function Home() {
-  const [birthDay, setBirthDay] = useState("");
-  // TODO: 今日の日付とかを適当に初期化して入れておく。
-  const [value, setValue] = useState("hu May 12 2022 00:00:00 GMT+0900 (日本標準時)");
+  const [invBirthDay, setInvBirthDay] = useState(null);
+  const [friendName, setFriendName] = useState("");
+  const calcInvBirthDay = (date: number, name) => {
+    // 逆誕生日を計算
+    const d = new Date(date);
+    console.log(
+      format(d, 'dd MMMM yyyy', { locale: ja }), name
+    );
+    const tInvBirthDay = addDays(d, 182);
+    console.log(tInvBirthDay);
+    setFriendName(name);
+    setInvBirthDay(tInvBirthDay);
+  };
   return (
-    <div className="ion-padding">
-      <div>
-        友達の誕生日を入力
-      </div>
-      <Button variant="contained" onClick={() => console.log(birthDay)}>Hello World</Button>
-      <LocalizationProvider dateAdapter={AdapterDateFns} locale={ja}>
-        <DatePicker
-          label="Basic example"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-            console.log(newValue);
-          }}
-          inputFormat='yyyy年MM月dd日'
-          mask='____年__月__日'
-          renderInput={(params) => <TextField {...params} />}
-        />
-       </LocalizationProvider>
-      <input onChange={(e) => setBirthDay(e.target.value)}></input>
-      <button onClick={() => console.log(birthDay)}>submit</button>
-      
-    </div>
+    <Container>
+      <Typography>
+        友達の逆誕生日を計算してみよう！
+      </Typography>
+      <InputModal calcInvBirthDay={calcInvBirthDay}
+                  setInvBirthDay={setInvBirthDay}
+                  setFriendName={setFriendName}
+                  ></InputModal>
+      {invBirthDay && <Typography>
+        {friendName}の次の逆誕生日は{format(invBirthDay, 'yyyy年MM月dd日', { locale: ja })}
+        </Typography>}
+    </Container>
   )
 }
